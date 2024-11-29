bc624dd7cabf5e116afb6536617acc04973da768
rafiradinka <rafi.radinka@gmail.com>
2024-11-29 20:32:54 +0700

    menambahkan kolom input html

--- views/barang.php
+++ views/barang.php
@@ -45,5 +45,41 @@ $brg = new Barang($connection);
           ?>                
         </table>
       </div>
+      <button type="button" class="btn btn-success" data-toggle="modal" data-target="#tambah">Tambah Data</button>
+
+      <div id="tambah" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal">&times;</button>
+              <h4 class="modal-title">Tambah Data Barang</h4>
+            </div>
+            <form action="" method="post" enctype="multipart/form-data">
+              <div class="modal-body">
+                <div class="form-group">
+                  <div class="control-label" for="nm_brg">Nama Barang</div>
+                  <input type="text" name="nm_brg" class="form-control" id="nm_brg" required>
+                </div>
+                <div class="form-group">
+                  <div class="control-label" for="hrg_brg">Harga Barang</div>
+                  <input type="number" name="hrg_brg" class="form-control" id="hrg_brg" required>
+                </div>
+                <div class="form-group">
+                  <div class="control-label" for="stok_brg">Stok Barang</div>
+                  <input type="number" name="stok_brg" class="form-control" id="stok_brg" required>
+                </div>
+                <div class="form-group">
+                  <div class="control-label" for="gbr_brg">Gambar Barang</div>
+                  <input type="file" name="gbr_brg" class="form-control" id="gbr_brg" required>
+                </div>
+              </div>
+              <div class="modal-footer">
+                <button type="reset" class="btn btn-danger">Reset</button>
+                <input type="submit" class="btn btn-success" name="tambah" value="Simpan">
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
   </div>
 </div>
